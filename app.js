0f1f3c06b63193b6fc211012c0cb238a5d98c1c7
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-08-10 11:23:51 +0500

    tesk

---
+++ app.js
@@ -0,0 +1,24 @@
+let arr = ["bat","tap","cat","tab","pat"]
+let arr1 = []
+
+function anagram () {
+    
+    for(i =0;i<=arr.length;i++){
+        arr1.push(arr[i+1])
+        if((arr[i]).length === arr[i+1].length ){
+            // for(arr1[1].length==arr1[i]){
+                
+            if((arr1[i])[])
+            
+
+            
+        }
+        else{
+            arr1.pop(arr[i+1])
+        }
+        console.log(arr1)
+    }
+}
+
+
+anagram()
